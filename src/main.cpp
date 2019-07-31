f5dfa3fd312578b6914d64c060ae43fae57e27b7
epandi <epandi@gmail.com>
2019-07-31 22:29:24 +0200

    add new switch BarKeuken

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,7 @@ NexSlider p0h1 = NexSlider(0, 6, "h1", &page0);       // Hobbyblinds slider
 NexDSButton p1bt0 = NexDSButton(1, 7, "bt0", &page1); // Buffetkast button
 NexDSButton p1bt1 = NexDSButton(1, 8, "bt1", &page1); // Whiskeykast button
 NexDSButton p1bt2 = NexDSButton(1, 9, "bt2", &page1); // Vensterbankvoor button
+NexDSButton p1bt3 = NexDSButton(1, 10, "bt3", &page1);// Barkeuken button
 
 NexNumber p2n0 = NexNumber(2, 7, "n0", &page2);       // Wasmachine voltage
 NexNumber p2n1 = NexNumber(2, 8, "n1", &page2);       // Wasmachine wattage
@@ -73,6 +74,7 @@ NexText p6t6 = NexText(6, 8, "t6", &page6);           // Hobbyblinds
 NexText p6t7 = NexText(6, 9, "t7", &page6);           // fleurblinds
 NexText p6t8 = NexText(6, 10, "t8", &page6);          // fleurcam
 NexText p6t9 = NexText(6, 11, "t9", &page6);          // buffetkast
+NexText p6t10 = NexText(6, 12, "t10", &page6);        // barkeuken
 
 NexRtc  rtc;
 
@@ -87,6 +89,7 @@ NexTouch *nex_listen_list[] = {
   &p1bt0,
   &p1bt1,
   &p1bt2,
+  &p1bt3,
   &p6t0,
   &page0,
   &page1,
@@ -157,6 +160,13 @@ void p1bt2PushCallback(void *ptr)
   uint32_t dual_state;
   p1bt2.getValue(&dual_state);
   if(dual_state) { MQTTClient.publish("cmnd/vensterbankvoor/POWER1", "ON"); } else { MQTTClient.publish("cmnd/vensterbankvoor/POWER1", "OFF"); }
+}
+void p1bt3PushCallback(void *ptr)
+{
+  // Barkeuken
+  uint32_t dual_state;
+  p1bt3.getValue(&dual_state);
+  if(dual_state) { MQTTClient.publish("cmnd/barkeuken/POWER1", "ON"); } else { MQTTClient.publish("cmnd/barkeuken/POWER1", "OFF"); }
 }
 void p6t0PushCallback(void *ptr)
 {
@@ -239,6 +249,13 @@ void callback(String topic, byte* message, unsigned int length) {
     const char* tempval = mqttjson["POWER"];
     if(strcmp(tempval, "ON") == 0){ p1bt2.setValue(1); } else if(strcmp(tempval, "OFF") == 0){ p1bt2.setValue(0); }
   }
+  else if((topic=="stat/barkeuken/RESULT") || (topic=="tele/barkeuken/STATE")){
+    // Vensterbankvoor button (button: p1bt3)
+    const char* tempval = mqttjson["POWER"];
+    dbSerial.print("aaaaaaaaaaa:");
+    dbSerial.print(tempval);
+    if(strcmp(tempval, "ON") == 0){ p1bt3.setValue(1); } else if(strcmp(tempval, "OFF") == 0){ p1bt3.setValue(0); }
+  }
   else if(topic=="tele/wasmachine/SENSOR"){
     // Wasmachine stats (text voltage: p2n0; text wattage: p2n1; text kwh today: p2t0; p2s0 waveform) || {"Time":"2019-07-01T19:30:53","ENERGY":{"TotalStartTime":"2018-11-24T22:05:23","Total":132.210,"Yesterday":0.000,"Today":0.000,"Period":0,"Power":0,"ApparentPower":0,"ReactivePower":0,"Factor":0.00,"Voltage":230,"Current":0.000}}
     p2n0.setValue(mqttjson["ENERGY"]["Voltage"]);
@@ -258,6 +275,7 @@ void callback(String topic, byte* message, unsigned int length) {
   else if (topic=="tele/hobbyblinds/LWT") { if(messageTemp=="Online") { p6t7.Set_background_color_bco(28651); } else { p6t7.Set_background_color_bco(64073); } }
   else if (topic=="tele/fleurcam/LWT") { if(messageTemp=="Online") { p6t8.Set_background_color_bco(28651); } else { p6t8.Set_background_color_bco(64073); } }
   else if (topic=="tele/buffetkast/LWT") { if(messageTemp=="Online") { p6t9.Set_background_color_bco(28651); } else { p6t9.Set_background_color_bco(64073); } }
+  else if (topic=="tele/barkeuken/LWT") { if(messageTemp=="Online") { p6t10.Set_background_color_bco(28651); } else { p6t10.Set_background_color_bco(64073); } }
 
   dbSerial.println();
 }
@@ -324,7 +342,7 @@ void setup(void) {
 
   pinMode(BUILTIN_LED, OUTPUT); digitalWrite (BUILTIN_LED, HIGH);
   // Start debuggin serial port
-  // dbSerial.begin(115200);
+  dbSerial.begin(115200);
   
   //Set the baudrate which is for debug and communicate with Nextion screen
   nexInit();
@@ -339,6 +357,7 @@ void setup(void) {
   p1bt0.attachPush(p1bt0PushCallback, &p1bt0);  // Buffetkast
   p1bt1.attachPush(p1bt1PushCallback, &p1bt1);  // Whiskeykast
   p1bt2.attachPush(p1bt2PushCallback, &p1bt2);  // Vensterbankvoor
+  p1bt3.attachPush(p1bt3PushCallback, &p1bt3);  // Barkeuken
   p6t0.attachPush(p6t0PushCallback, &p6t0);     // enable/disable Debugging
   page0.attachPush(page0PushCallback, &page0);  // Page0 Blinds
   page1.attachPush(page1PushCallback, &page1);  // Page1 Switches
